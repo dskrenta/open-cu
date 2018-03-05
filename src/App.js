3b27847e75164f46a5ad6b30feee8ccc666f7e76
dskrenta <dskrenta@gmail.com>
2018-03-05 10:14:12 -0800

    first version with sample data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import opening_hours from 'opening_hours';
+
+import data from './data.json';
 import './App.css';
 
 class App extends Component {
   render() {
+    const currentState = data.cuDining.map(item => {
+      const oh = new opening_hours(item.openingHours);
+      return oh.getState();
+    });
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Open CU</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <ul>
+            {data.cuDining.map((item, index) => (
+              <li key={index}>{item.title} &middot; {currentState[index] ? 'OPEN' : 'CLOSED'}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
