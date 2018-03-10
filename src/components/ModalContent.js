be406979a2c07d2f2a5c4eb77c8895bcb38e80d8
BDesBrisay <bryce@desbrisay.com>
2018-03-09 19:19:31 -0700

    added icons to modal content

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as MaterialIcon from 'react-icons/lib/md';
 
 import HoursTable from './HoursTable';
 
@@ -17,22 +18,41 @@ const ModalContent = ({ item }) => (
         : <div className="modalClosedIndicator"></div>}
     </div>
     <div className="timeTableContain">
-      {item.overview && <div className="row">
+      {item.overview && <div className="overview">
         <p>{item.overview}</p>
       </div>}
       {item.phone && <div className="row">
-        <p>Call: {item.phone}</p>
+        <p>
+          <MaterialIcon.MdPhone size={18} /> 
+          {item.phone}
+        </p>
       </div>}
       {item.address && <div className="row">
-        <p>{item.address}</p>
+        <p>
+          <MaterialIcon.MdPlace size={18} /> 
+          {item.address}
+        </p>
       </div>}
       {item.menu && <div className="row">
-        <a href={item.menu}><p>View Menu</p></a>
+        <p>
+          <MaterialIcon.MdRestaurantMenu size={18} /> 
+          <a href={item.menu}>View Menu</a>
+        </p>
       </div>}
       {item.order && <div className="row">
-        <p className="row">Order Online:&nbsp;&nbsp;{item.order.map((orderItem, index) => (
-          <a key={index} href={orderItem.url}><span>{orderItem.title}</span></a>
-        ))}</p>
+        <p className="row">
+          <MaterialIcon.MdLocalShipping size={18} />
+          &nbsp;
+          {item.order.map((orderItem, index) => (
+            <a key={index} href={orderItem.url}><span>{orderItem.title}</span></a>
+          ))}
+        </p>
+      </div>}
+      {item.paymentAccepted && <div className="row">
+        <p>
+          <MaterialIcon.MdMonetizationOn size={18} style={{marginBottom: 2}} /> 
+          {item.paymentAccepted}
+        </p>
       </div>}
       {item.hoursTable && <HoursTable hours={item.hoursTable} />}
     </div>
