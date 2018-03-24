0143f2373ca6556e4d4bc66d87c2ef20070b9a27
BDesBrisay <bryce@desbrisay.com>
2018-03-24 00:21:55 -0700

    added images to some feed items

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -5,6 +5,7 @@ import formatNext from '../utils/formatNext';
 
 const FeedItem = ({ item, hours }) => (
   <div className="feedItem">
+    {item.imageUrl && <img src={item.imageUrl} className="itemImage" alt="" />}
     <div className="textContain">
       <h4 className="mainText">{item.title}</h4>
       {item.address && <p className="subText">{item.address}</p>}
