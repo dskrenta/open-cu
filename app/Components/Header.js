e894044c1cd75eba5ee52404a447bdc5d7673a07
BDesBrisay <bryce@desbrisay.com>
2018-04-16 06:13:27 -0600

    added weather to header and modal

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, Text, Image, StyleSheet, Platform, TouchableHighlight } from 'react-native';
 import { ifIphoneX } from 'react-native-iphone-x-helper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Header = ({ modalHandler }) => (
-  <View style={styles.contain}>
-    <Text style={styles.title}>Open CU</Text>
-    <Text>Weather</Text>
-  </View>
-)
+import getIcon from '../utils/getIcon';
+import Weather from './Weather';
+
+class Header extends React.Component {
+  state = {
+    weather: undefined
+  }
+
+  componentDidMount() {
+    const unit = 'F';
+    const location = 'Boulder, CO'
+    const queryUri = `https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where u='${unit}' AND woeid in (select woeid from geo.places(1) where text="${location}")&format=json`;
+    fetch(queryUri)
+      .then((res) => {
+        if (res.status >= 400) throw new Error('Bad response from server');
+        return(res.json());
+      })
+      .then(
+        (json) => json.query.results === null ? null : this.setState({weather: json.query.results.channel})
+      )
+  }
+
+  openWeather = () => {
+    this.props.modalHandler(true, (<Weather weather={this.state.weather} />));
+  }
+
+  render() {
+    const { modalHandler } = this.props;
+    return (
+      <View style={styles.contain}>
+        <Text style={styles.title}>Open CU</Text>
+        {this.state.weather &&
+          <TouchableHighlight onPress={this.openWeather} underlayColor='transparent'>
+            <View style={styles.weatherContain}>
+              <Icon style={styles.weatherIcon} name={getIcon(this.state.weather.item.condition.code)} size={30} />
+              <Text style={styles.weatherText}>{this.state.weather.item.condition.temp}&deg;</Text>
+            </View>
+          </TouchableHighlight>
+        }
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   contain: {
@@ -40,6 +78,21 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold'
+  },
+  weatherContain: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 5
+  },
+  weatherText: {
+    color: '#fff',
+    fontSize: 24,
+    marginLeft: 5
+  },
+  weatherIcon: {
+    height: 30,
+    width: 30,
+    color: 'white'
   }
 })
 
--- app/Components/Weather.js
+++ app/Components/Weather.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Weather = () => (
-  <View></View>
+import getIcon from '../utils/getIcon';
+
+const Weather = ({ weather }) => (
+  <View>
+    <View style={styles.header}>
+      <Text style={styles.location}>Boulder, CO</Text>
+      <View style={styles.row}>
+        <Icon name={getIcon(weather.item.condition.code)} size={65} />
+        <Text style={styles.temp}>{weather.item.condition.temp}&deg;</Text>
+      </View>
+      <Text style={styles.location}>{weather.item.condition.text}</Text>
+    </View>
+    <View style={styles.body}>
+      {weather.item.forecast.map((day, index) => (
+        <View key={index} style={styles.forecastRow}>
+          <Text>{day.day}</Text>
+          <Text>{}</Text>
+          <Text>{day.high}</Text>
+          <Text>{day.low}</Text>
+        </View>
+      ))}
+    </View>
+  </View>
 )
 
 const styles = StyleSheet.create({
-
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30
+  },
+  location: {
+    fontSize: 18
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  forecastRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 1
+  },
+  temp: {
+    fontSize: 65,
+    fontWeight: 'bold',
+    margin: 10,
+    marginRight: 0
+  },
+  body: {
+    display: 'flex',
+  }
 })
 
 export default Weather;
